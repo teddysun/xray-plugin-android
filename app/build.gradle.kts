985a46f7fe6a229c8b51e354f21a5b05bd788525
Teddysun <i@teddysun.com>
2022-11-07 13:19:21 +0900

    Bump version v1.6.3
    Signed-off-by: Teddysun <i@teddysun.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.github.shadowsocks.plugin.xray"
         minSdkVersion(23)
         targetSdkVersion(30)
-        versionCode = 1060200
-        versionName = "1.6.2"
+        versionCode = 1060300
+        versionName = "1.6.3"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
